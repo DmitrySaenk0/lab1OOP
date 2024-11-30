a941d320223e71b75ba0a32120775853d563b3a3
Dmitry <randomemail@example.com>
2024-11-30 20:45:12 +0200

    circle calculation

--- app/src/main/java/com/example/lab1_oop/MainActivity.kt
+++ app/src/main/java/com/example/lab1_oop/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.lab1_oop
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.math.PI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputRadius: EditText = findViewById(R.id.inputRadius)
+        val calculateButton: Button = findViewById(R.id.calculateButton)
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+
+        calculateButton.setOnClickListener {
+            val radiusText = inputRadius.text.toString()
+
+            if (radiusText.isNotEmpty()) {
+                val radius = radiusText.toDoubleOrNull()
+
+                if (radius != null && radius > 0) {
+                    val area = PI * radius * radius
+                    resultTextView.text = "Площа кола: %.2f".format(area)
+                } else {
+                    resultTextView.text = "Введіть правильне значення радіуса!"
+                }
+            } else {
+                resultTextView.text = "Поле радіуса не може бути порожнім!"
+            }
         }
-        print("Goodbye Word!")
     }
 }
